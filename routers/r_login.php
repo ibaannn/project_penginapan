ceb33031944664172a09f241b0e8b42daeaaee9c
ibaannn <luckysnow995@gmail.com>
2023-08-21 11:50:51 +0700

    inisialisasi pertama

---
+++ routers/r_login.php
@@ -0,0 +1,43 @@
+<?php 
+
+include "../controllers/c_login.php";
+
+
+
+$login = new c_login();
+
+try {
+    if($_GET["aksi"] == "register") {
+        $id = $_POST["id"];
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $role = $_POST["role"];
+        $photo = $_POST["photo"];
+    
+        $pass = password_hash($password, PASSWORD_DEFAULT);
+    
+        $login->register($id, $username, $email, $pass, $role, $photo);
+        if($login) {
+            echo "<script> alert('akun berhasil register');
+            document.location.href = '../views/index.php';
+            </script>";
+        }
+    }elseif ($_GET["aksi"] == "login") {
+        $email = $_POST["email"];
+        $pass = $_POST["pass"];
+    
+        $login->login($email, $pass);
+    }elseif ($_GET["aksi"] == "logout") {
+        $login->logout();
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
+
+
+
+
+?>
